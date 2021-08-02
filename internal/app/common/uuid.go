095550342dec38d24f5589633ce5c0857ebc59eb
Panayiotis Kritiotis <p.kritiotis@thebeat.co>
2021-08-02 23:02:43 +0300

    Added time and uuid providers for testing purposes

---
+++ internal/app/common/uuid.go
@@ -0,0 +1,21 @@
+package common
+
+import "github.com/google/uuid"
+
+// UUIDProvider abstracts the uuid generation
+type UUIDProvider interface {
+	NewUUID() uuid.UUID
+}
+
+// NewUUIDProvider constructor that returns default uuid generation
+func NewUUIDProvider() UUIDProvider {
+	return uuidProvider{}
+}
+
+type uuidProvider struct {
+}
+
+// NewUUID generates a new UUID
+func (u uuidProvider) NewUUID() uuid.UUID {
+	return uuid.New()
+}
